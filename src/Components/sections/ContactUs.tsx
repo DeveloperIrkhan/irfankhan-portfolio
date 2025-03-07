96b275d9520241a79c519bf3fb47da37feec4ec7
DeveloperIRkhan <programmer.irfansha@hotmail.com>
2025-03-07 14:23:37 +0500

    website completed

--- src/Components/sections/ContactUs.tsx
+++ src/Components/sections/ContactUs.tsx
@@ -1,55 +1,130 @@
+import { useState } from "react";
 import Title from "../Title";
-
+import emailjs from "emailjs-com";
 const ContactUs = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: ""
+  });
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const res = await emailjs.sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        e.target as HTMLFormElement,
+        import.meta.env.VITE_PUBLIC_KEY
+      );
+      console.log(res);
+    } catch (error) {
+      console.log("error", error);
+    } finally {
+      setIsLoading(false);
+      setFormData({ name: "", email: "", phone: "", message: "" });
+    }
+  };
   return (
-    <div data-aos="zoom-in" aos-duration="3000"
+    <div
+      data-aos="zoom-in"
+      aos-duration="3000"
       id="contact"
       className="min-h-screen flex flex-col items-center justify-center py-20"
     >
       <div className="max-w-5xl mx-auto px-4">
-        <Title data-aos="fade-down-right" aos-duration="2000" title="Reach me Out" />
+        <Title
+          data-aos="fade-down-right"
+          aos-duration="2000"
+          title="Reach me Out"
+        />
       </div>
-      <form action="" className="p-3 flex flex-col w-96 gap-y-3">
-        <input 
-        data-aos="fade-up" aos-duration="2000" 
-        type="text" className="font-Outfit rounded p-2 w-full bg-white/10 
+      <form
+        onSubmit={onSubmitHandler}
+        className="p-3 flex flex-col w-96 gap-y-3"
+      >
+        <input
+          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          value={formData.name}
+          data-aos="fade-up"
+          aos-duration="2000"
+          type="text"
+          className="font-Outfit rounded p-2 w-full bg-white/10 
         border border-white/30 transition focus:outline-none focus:border-amber-400
         focus:bg-white/40"
-        typeof="text"
-        required
-        name="name"
-        id="name"
-        placeholder="Please enter your name"/>
-        <input 
-        data-aos="fade-up" aos-duration="4000" 
-        type="text" className="font-Outfit rounded p-2 w-full bg-white/10 
+          typeof="text"
+          required
+          name="name"
+          id="name"
+          placeholder="Please enter your name"
+        />
+        <input
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+          value={formData.email}
+          data-aos="fade-up"
+          aos-duration="4000"
+          type="text"
+          className="font-Outfit rounded p-2 w-full bg-white/10 
         border border-white/30 transition focus:outline-none focus:border-amber-400
         focus:bg-white/40"
-        typeof="text"
-        required
-        name="email"
-        id="email"
-        placeholder="Please enter your email"/>
-        <input 
-        data-aos="fade-up" aos-duration="6000" 
-        type="text" className="font-Outfit rounded p-2 w-full bg-white/10 
+          typeof="text"
+          required
+          name="email"
+          id="email"
+          placeholder="Please enter your email"
+        />
+        <input
+          onChange={(e) => setFormData({ ...formData, phone: e.target.value })}
+          value={formData.phone}
+          data-aos="fade-up"
+          aos-duration="6000"
+          type="text"
+          className="font-Outfit rounded p-2 w-full bg-white/10 
         border border-white/30 transition focus:outline-none focus:border-amber-400
         focus:bg-white/40"
-        typeof="text"
-        required
-        name="Phone"
-        id="Phone"
-        placeholder="Please enter your phone"/>
-        <textarea 
-        data-aos="fade-up" aos-duration="8000" 
-        type="text" className="font-Outfit rounded p-2 h-[100px] w-full bg-white/10 
+          typeof="text"
+          required
+          name="phone"
+          id="phone"
+          placeholder="Please enter your phone"
+        />
+        <textarea
+          onChange={(e) =>
+            setFormData({ ...formData, message: e.target.value })
+          }
+          value={formData.message}
+          data-aos="fade-up"
+          aos-duration="8000"
+          className="font-Outfit rounded p-2 h-[100px] w-full bg-white/10 
         border border-white/30 transition focus:outline-none focus:border-amber-400
         focus:bg-white/40"
-        typeof="text"
-        required
-        name="message"
-        id="message"
-        placeholder="Please enter your message"/>
+          typeof="text"
+          required
+          name="message"
+          id="message"
+          placeholder="Please enter your message"
+        />
+
+        <button
+          type="submit"
+          className="relative border-amber-400 border-2 font-Outfit text-amber-400 py-3 px-6 rounded font-medium overflow-hidden
+          duration-400 hover:translate-y-1 hover:shadow-[0_0_25px_rgba(255,165,0,0.7)] cursor-pointer
+          hover:border-amber-500 hover:text-amber-500"
+        >
+          Send Me a Message{" "}
+          {isLoading ? (
+            <img
+              className="w-8 absolute top-2.5 right-14"
+              src="../src/assets/Spinner.svg"
+            />
+          ) : (
+            ""
+          )}
+        </button>
       </form>
     </div>
   );
--- src/Components/sections/Home.tsx
+++ src/Components/sections/Home.tsx
@@ -5,27 +5,27 @@ const Home = () => {
       className="min-h-screen flex items-center justify-center relative"
     >
       <div className="text-center">
-        <h1 data-aos="flip-right" className="md:text-5xl text-2xl font-bold font-Outfit mb-4 bg-gradient-to-bl from-amber-400 to-gray-200 bg-clip-text text-transparent">
+        <h1 className="md:text-5xl text-2xl font-bold font-Outfit mb-4 bg-gradient-to-bl from-amber-400 to-gray-200 bg-clip-text text-transparent">
           Welcome! I'm IrfanKhan
         </h1>
-        <h1 data-aos="flip-right" className="md:text-2xl text-md font-bold font-Outfit mb-4 bg-gradient-to-tl from-amber-400 to-gray-200 bg-clip-text text-transparent">
+        <h1 className="md:text-2xl text-md font-bold font-Outfit mb-4 bg-gradient-to-tl from-amber-400 to-gray-200 bg-clip-text text-transparent">
           a {"<"}Web developer{"/>"}
         </h1>
 
-        <p data-aos="flip-left" className=" text-gray-300  font-Outfit md:text-xl text-center text-sm tracking-[3px]  mb-0 md:max-w-lg max-w-[80%] mx-auto">
+        <p className=" text-gray-300  font-Outfit md:text-xl text-center text-sm tracking-[3px]  mb-0 md:max-w-lg max-w-[80%] mx-auto">
           I'm a full-stack developer passionate about crafting clean, scalable
           web applications. My goal is to create solutions that deliver high
           performance and an exceptional user experience.
         </p>
         <div className="flex justify-center gap-4 mt-5">
-          <a data-aos="fade-right"
+          <a
             href="#projects"
             className="bg-amber-400 font-Outfit text-white py-3 px-6 rounded font-medium transition relative overflow-hidden
           duration-400 hover:translate-y-2 hover:shadow-[0_0_25px_rgba(255,165,0,0.7)] cursor-pointer hover:bg-amber-500"
           >
             view my work
           </a>
-          <a data-aos="fade-left"
+          <a
             href="#contact"
             className="border border-amber-400 font-Outfit text-amber-400 py-3 px-6 rounded font-medium transition relative overflow-hidden
           duration-400 hover:translate-y-2 hover:shadow-[0_0_25px_rgba(255,165,0,0.7)] cursor-pointer
